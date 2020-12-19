da8a625736460646c40d2e50de3b329afae6ff1c
Richard Larancuente <richardllara05@gmail.com>
2020-12-18 22:03:09 -0500

    added glfw and events

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,17 +1,11 @@
 #pragma once
 
-/* BUG: Cannot haved nested ifdef with __declspec */
-
-//#ifdef HZ_PLATFORM_WINDOWS
-//	#ifdef HZ_BUILD_DLL
-//		#define HAZEL_API  __declspec(dllexport)
-//	#else
-//		#define HAZEL_API  __declspec(dllimport)
-//	#endif
-//#endif
-
-#ifdef HZ_BUILD_DLL
-	#define HAZEL_API    __declspec(dllexport)
-#else
-	#define HAZEL_API    __declspec(dllimport)
+#ifdef HZ_PLATFORM_WINDOWS
+	#ifdef HZ_BUILD_DLL
+		#define HAZEL_API  __declspec(dllexport)
+	#else
+		#define HAZEL_API  __declspec(dllimport)
+	#endif
 #endif
+
+#define BIT(x) (1 << x)
